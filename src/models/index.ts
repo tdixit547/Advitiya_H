7912b409a3af6c0dd84d59e685386eb24488eb4b
Tanmay Dixit <tanmaydixit547@gmail.com>
2026-01-24 18:33:54 +0530

    cleanup: Remove duplicate src and backend_branch_copy folders

--- src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './LinkHub';
-export * from './RuleTree';
-export * from './Variant';
-export * from './VariantStats';
-export * from './Event';
-export * from './User';
--- src/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DecisionTreeEngine';
-export * from './VariantResolver';
-export * from './CacheService';
-export * from './EventLogger';
-export * from './ScoringService';
-export * from './AuthService';
